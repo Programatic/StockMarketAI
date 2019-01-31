1e2fcd9021937c609505e0bc0c856bc3ad5891de
Ford <xtreameprogram2@gmail.com>
2019-01-30 22:26:48 -0500

    Fixing

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import numpy as np
 
 #to plot within notebook
 import matplotlib.pyplot as plt
-%matplotlib inline
+# %matplotlib inline
 
 #setting figure size
 from matplotlib.pylab import rcParams
@@ -16,7 +16,7 @@ from sklearn.preprocessing import MinMaxScaler
 scaler = MinMaxScaler(feature_range=(0, 1))
 
 #read the file
-df = pd.read_csv('NSE-TATAGLOBAL.csv')
+df = pd.read_csv('EOD-MCD.csv')
 
 #print the head
 df.head()
@@ -30,29 +30,73 @@ plt.plot(df['Close'], label='Close Price history')
 
 
 #%%
-#creating dataframe with date and the target variable
+from sklearn.preprocessing import MinMaxScaler
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, LSTM
+
+#creating dataframe
 data = df.sort_index(ascending=True, axis=0)
 new_data = pd.DataFrame(index=range(0,len(df)),columns=['Date', 'Close'])
-
 for i in range(0,len(data)):
-     new_data['Date'][i] = data['Date'][i]
-     new_data['Close'][i] = data['Close'][i]
+    new_data['Date'][i] = data['Date'][i]
+    new_data['Close'][i] = data['Close'][i]
 
 #%%
-#splitting into train and validation
-train = new_data[:987]
-valid = new_data[987:]
-
-#make predictions
-preds = []
-for i in range(0,248):
-    a = train['Close'][len(train)-248+i:].sum() + sum(preds)
-    b = a/248
-    preds.append(b)
+#setting index
+new_data.index = new_data.Date
+new_data.drop('Date', axis=1, inplace=True)
 
 #%%
-#plot
-valid['Predictions'] = 0
-valid['Predictions'] = preds
+#creating train and test sets
+dataset = new_data.values
+
+train = dataset[0:1000,:]
+valid = dataset[1000:,:]
+
+#%%
+#converting dataset into x_train and y_train
+scaler = MinMaxScaler(feature_range=(0, 1))
+scaled_data = scaler.fit_transform(dataset)
+
+x_train, y_train = [], []
+for i in range(60,len(train)):
+    x_train.append(scaled_data[i-60:i,0])
+    y_train.append(scaled_data[i,0])
+x_train, y_train = np.array(x_train), np.array(y_train)
+
+x_train = np.reshape(x_train, (x_train.shape[0],x_train.shape[1],1))
+
+#%%
+# create and fit the LSTM network
+model = Sequential()
+model.add(LSTM(units=50, return_sequences=True, input_shape=(x_train.shape[1],1)))
+model.add(LSTM(units=50))
+model.add(Dense(1))
+
+model.compile(loss='mean_squared_error', optimizer='adam')
+model.fit(x_train, y_train, epochs=1, batch_size=1, verbose=2)
+
+#predicting 246 values, using past 60 from the train data
+inputs = new_data[len(new_data) - len(valid) - 60:].values
+inputs = inputs.reshape(-1,1)
+inputs  = scaler.transform(inputs)
+
+X_test = []    
+for i in range(60,inputs.shape[0]):
+    X_test.append(inputs[i-60:i,0])
+X_test = np.array(X_test)
+
+X_test = np.reshape(X_test, (X_test.shape[0],X_test.shape[1],1))
+closing_price = model.predict(X_test)
+closing_price = scaler.inverse_transform(closing_price)
+
+
+#%%
+train = new_data[:1000]
+valid = new_data[1000:]
+valid['Predictions'] = closing_price
 plt.plot(train['Close'])
-plt.plot(valid[['Close', 'Predictions']])
+plt.plot(valid[['Close','Predictions']])
+
+rms=np.sqrt(np.mean(np.power((valid['Predictions'].values-closing_price),2)))
+print(rms)
