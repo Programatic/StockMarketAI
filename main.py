6785527d1717ba18662f12f24a0f49e05fa60063
Programatic <xtreameprogram2@gmail.com>
2019-02-01 15:41:02 -0500

    eggsdee

--- main.py
+++ main.py
@@ -4,13 +4,20 @@ import pandas as pd
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, LSTM
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
 
+raw = pd.read_csv("EOD-MCD.csv").sort_index(ascending=False)
+raw['Date'] = pd.to_datetime(raw['Date'], format="%Y-%m-%d")
+raw.index = raw['Date']
 
-data= pd.read_csv("EOD-MCD.csv")
-data['Date'] = pd.to_datetime(data['Date'], format="%Y-%m-%d")
-data.index = data['Date']
-
+data = pd.DataFrame(index=range(0, len(raw)), data=raw.loc[:, ['Date', 'Close']].values, columns=['Date', 'Price'])
 #%% Plots and lists the data that will be used for training and testing
-plt.plot(data['Close'])
+plt.plot(data['Date'], data['Price'])
 
 data.head() #just verify that we read the data properly
+
+#%% Scaling and dividing the data into test and train
+scaler = MinMaxScaler()
+
+train_data = data.loc[:3095, 'Price']
+test_data = data.loc[3095:, 'Price']
